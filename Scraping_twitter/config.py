cfa051c3e83241927924979658aacb3087770d52
Alief Zulvan <depanb187@gmail.com>
2024-01-30 01:31:04 +0700

    first init

---
+++ Scraping_twitter/config.py
@@ -0,0 +1,5 @@
+# edit this 
+consumer_key = '2aukMPlEiUdKvVfldyVScHikG'
+consumer_secret = '6P0hFmQjRhKrh7uCNXozg4Tx7rVCXQJN5aJb1e4qWRrfY1HO6s'
+access_token = '1245212494756372480-JTPEBh7qB935HTfy7jJvwz6AlRIYrA'
+access_secret = 'neA4apdl3958t1UHpm6n1venOhBTYnBfnxypn7lqXNOHG'
